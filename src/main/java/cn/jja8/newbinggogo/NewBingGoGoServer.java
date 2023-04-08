1a4598031dd76817d2c15ae1502dee79a905a6e4
jayshen2015 <jayshen1986@gmail.com>
2023-04-08 14:11:44 +0800

    edit

--- src/main/java/cn/jja8/newbinggogo/NewBingGoGoServer.java
+++ src/main/java/cn/jja8/newbinggogo/NewBingGoGoServer.java
@@ -40,6 +40,7 @@ public class NewBingGoGoServer extends NanoWSD {
         }
         String ip = new Date()+":"+getIp(session);
         String url = session.getUri();
+        return getReturnError(url);
         if(url.equals("/turing/conversation/create")){//创建聊天
             System.out.println(ip+":请求创建聊天");
             return goUrl(session,"https://www.bing.com/turing/conversation/create");
